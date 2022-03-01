be48398774132d785651dfdca91cdf7f3766fd7c
Superdash <pablorsantana@hotmail.com>
2022-03-01 15:20:25 -0300

    Estrutura HTML e rotina de captura/formatação de horário

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//Aqui vai o código
+let date = new Date();
+var horario;
+
+let data = document.getElementById("data");
+
+function load(){
+    getTime();
+    horario = setInterval(getTime, 1000);
+
+    let hora = document.getElementById("hora");
+    hora.innerHTML = `Hoje é ${new Intl.DateTimeFormat('pt-br', {dateStyle: "full"}).format(date)}`;
+}
+
+function getTime(){
+    date = new Date();
+    data.innerHTML = `${new Intl.DateTimeFormat('pt-br', {timeStyle: "medium"}).format(date)}`;
+}
